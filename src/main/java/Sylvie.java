c5c0c502163be7e5a0ef38fa2cf712a5e89e635b
biscake <leonut.ljh@gmail.com>
2025-08-20 22:00:18 +0800

    Add private static Textbox class
    Textbox class have a print function to wrap message in a border
    Helper function to print border

---
+++ src/main/java/Sylvie.java
@@ -0,0 +1,46 @@
+public class Sylvie {
+    private static final String BOT_NAME = "Sylvie";
+
+    private static class Textbox {
+        private final String message;
+
+        public Textbox(String message) {
+            this.message = message;
+        }
+
+        /**
+         * Prints the message inside a border.
+         */
+        public void print() {
+            String[] lines = message.split("\n");
+            int maxWidth = Integer.MIN_VALUE;
+
+            for (String s : lines) {
+                maxWidth = Math.max(s.length(), maxWidth);
+            }
+
+            printBorder(maxWidth);
+            System.out.println(message);
+            printBorder(maxWidth);
+        }
+
+        /**
+         * Helper function to print border for text box.
+         * 
+         * @param width length of the border to print.
+         */
+        private void printBorder(int width) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < width; i++) {
+                sb.append("_");
+            }
+
+            sb.append("\n");
+
+            System.out.println(sb);
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
