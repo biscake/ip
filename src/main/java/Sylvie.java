1de6c1aeef044b4538f89791bf373972289c772b
biscake <leonut.ljh@gmail.com>
2025-08-21 12:16:55 +0800

    Add indentation for Textbox print
    Implement indent function that returns a string of whitespaces

--- src/main/java/Sylvie.java
+++ src/main/java/Sylvie.java
@@ -12,25 +12,32 @@ public class Sylvie {
          * Prints the message inside a border.
          */
         public void print() {
+            int INDENT_SIZE = 4;
             String[] lines = message.split("\n");
             int maxWidth = Integer.MIN_VALUE;
 
-            for (String s : lines) {
+            for (int i = 0; i < lines.length; i++) {
+                String s = lines[i];
                 maxWidth = Math.max(s.length(), maxWidth);
+                StringBuilder sb = new StringBuilder(s);
+                lines[i] = sb.insert(0, indent(INDENT_SIZE)).toString();
             }
 
-            printBorder(maxWidth);
-            System.out.println(message);
-            printBorder(maxWidth);
+            printBorder(maxWidth, INDENT_SIZE);
+            System.out.println(String.join("\n", lines));
+            printBorder(maxWidth, INDENT_SIZE);
         }
 
         /**
-         * Helper function to print border for text box.
+         * Prints a border of underscores with the specified indentation size.
          * 
-         * @param width length of the border to print.
+         * @param width width of the length of the border to print.
+         * @param indentSize the number of spaces to indent before the border.
          */
-        private void printBorder(int width) {
+        private void printBorder(int width, int indentSize) {
             StringBuilder sb = new StringBuilder();
+
+            sb.append(indent(indentSize));
             for (int i = 0; i < width; i++) {
                 sb.append("_");
             }
@@ -38,6 +45,22 @@ public class Sylvie {
             sb.append("\n");
 
             System.out.println(sb);
+        }
+
+        /**
+         * Helper function that returns string of whitespace
+         * 
+         * @param size Length of the indentation
+         * @return String of whitespaces of indentation size
+         */
+        private String indent(int size) {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < size; i++) {
+                sb.append(" ");
+            }
+
+            return sb.toString();
         }
     }
 
