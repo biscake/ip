8790c16598d06da4e5159c00f37e6aa06c3cc0a7
biscake <leonut.ljh@gmail.com>
2025-08-21 12:24:59 +0800

    Move Textbox class to a separate file
    Textbox class cannot be used by other classes since it is a private
    static class.

--- src/main/java/Sylvie.java
+++ src/main/java/Sylvie.java
@@ -1,69 +1,6 @@
 public class Sylvie {
     private static final String BOT_NAME = "Sylvie";
 
-    private static class Textbox {
-        private final String message;
-
-        public Textbox(String message) {
-            this.message = message;
-        }
-
-        /**
-         * Prints the message inside a border.
-         */
-        public void print() {
-            int INDENT_SIZE = 4;
-            String[] lines = message.split("\n");
-            int maxWidth = Integer.MIN_VALUE;
-
-            for (int i = 0; i < lines.length; i++) {
-                String s = lines[i];
-                maxWidth = Math.max(s.length(), maxWidth);
-                StringBuilder sb = new StringBuilder(s);
-                lines[i] = sb.insert(0, indent(INDENT_SIZE)).toString();
-            }
-
-            printBorder(maxWidth, INDENT_SIZE);
-            System.out.println(String.join("\n", lines));
-            printBorder(maxWidth, INDENT_SIZE);
-        }
-
-        /**
-         * Prints a border of underscores with the specified indentation size.
-         * 
-         * @param width width of the length of the border to print.
-         * @param indentSize the number of spaces to indent before the border.
-         */
-        private void printBorder(int width, int indentSize) {
-            StringBuilder sb = new StringBuilder();
-
-            sb.append(indent(indentSize));
-            for (int i = 0; i < width; i++) {
-                sb.append("_");
-            }
-
-            sb.append("\n");
-
-            System.out.println(sb);
-        }
-
-        /**
-         * Helper function that returns string of whitespace
-         * 
-         * @param size Length of the indentation
-         * @return String of whitespaces of indentation size
-         */
-        private String indent(int size) {
-            StringBuilder sb = new StringBuilder();
-
-            for (int i = 0; i < size; i++) {
-                sb.append(" ");
-            }
-
-            return sb.toString();
-        }
-    }
-
     private static void greet() {
         String text = "Hello! I'm Sylvie\nWhat can I do for you?";
         new Textbox(text).print();
