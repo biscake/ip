dc7bc0fdf38c23337dba23030a539f31f5c3b082
biscake <leonut.ljh@gmail.com>
2025-08-21 13:23:10 +0800

    Add Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,24 @@
+public class Task {
+  private final String description;
+  private boolean isDone;
+
+  public Task(String description) {
+    this.description = description;
+    this.isDone = false;
+  }
+
+  public void markDone() {
+    this.isDone = true;
+  }
+
+  public void markNotDone() {
+    this.isDone = false;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append(isDone ? "[X]" : "[ ]").append(" ").append(this.description);
+    return sb.toString();
+  }
+}
